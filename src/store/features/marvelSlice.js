d5c0d12a916298baf4c83b07620b4e08e062d08f
Mario Posada <marioposadamiranda@gmail.com>
2023-02-01 20:46:51 -0300

    Add api

--- src/store/features/marvelSlice.js
+++ src/store/features/marvelSlice.js
@@ -1,20 +1,23 @@
 import {createSelector,createEntityAdapter} from '@reduxjs/toolkit'
+import { marvelApi } from '../services/marvelApi';
+import apiParams from "./../../utils/marvelHash";
 
 const marvelAdapter = createEntityAdapter()
 
 const initialState = marvelAdapter.getInitialState()
 
-export const marvelSlice = createSlice({
-  name: "marvel",
-  initialState: {
-    marvel: [{ name: "Spiderman" }, { name: "Ironman" }],
-  },
-  reducers: {
-    setMarvel: (state, action) => {
-      state.marvel = action.payload;
-    },
-  },
-});
+export const extendedMarvelSlice = marvelApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getCharacters: builder.query({
+      query: () => {
+        return {
+          url: `characters?ts=${apiParams.ts}&apikey=${apiParams.apikey}&hash=${apiParams.hash}`,
+        };
+      }
+    }),
+  }),
+  overrideExisting: false,
+})
 
 export const { setMarvel } = marvelSlice.actions;
 
